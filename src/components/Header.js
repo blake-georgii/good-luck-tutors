9ea049a398fa08576fae18b032730e7a28b02cb3
Blake Georgii <blakegeorgii@gmail.com>
2022-03-20 14:20:53 -0700

    added related files for header and jumbotron

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <nav className="navbar sticky-top navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Good Luck Tutors</a>
+                    <a href = "#contact" class="btn btn-sm btn-color justify-content-end">Contact Us</a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Jumbotron() {
+    return (
+        <div className='jumbotron img-fluid p-5 text-white'>
+            <h3>Luck Favors the prepared</h3>
+            <h4>-Louis Pasteur</h4>
+        </div>
+    );
+}
+
+export default Jumbotron;
